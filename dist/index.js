e88feb848b7c4502f874b09fab89d9ca5f6659f3
Hatef <sz.hatef@gmail.com>
2020-11-08 13:15:04 +0300

    my server

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 app.get('/', function (req, res) {
-    res.json({ result: "ok" });
+    res.json({ result: "ok?" });
 });
 app.listen(5000, function () {
     console.log('server started on port 5000');
