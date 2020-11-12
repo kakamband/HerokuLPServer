d8910df2c30842dbe8bc67dc99afad223b1bba35
Hatef <sz.hatef@gmail.com>
2020-11-12 20:19:37 +0300

    my server

--- src/RNA/RNAx549.ts
+++ src/RNA/RNAx549.ts
@@ -8,9 +8,9 @@ export function gene ( user: u.user, ribosome: g.Ribosome ): Promise<g.gene> {
 
     return new Promise ( (rs, rx) => { 
         
-        let DNA = require( "../DNA/DNAx" + ribosome.code + ".ts" ).DNA;
+        let DNA = require( "../DNA/DNAx" + ribosome.code + ".ts" );
         
-        a_good_gene_4_user( user, DNA, "start" )
+        a_good_gene_4_user( user, DNA.DNA, "start" )
         .then( gene => rs( gene ) )
         .catch( err => rx( err ) );
     
