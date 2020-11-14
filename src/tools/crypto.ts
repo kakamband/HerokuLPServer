31b3d12035635a22f49fb6b4ab8bfbf0e583539e
Hatef <sz.hatef@gmail.com>
2020-11-14 21:22:38 +0300

    my server

--- src/tools/crypto.ts
+++ src/tools/crypto.ts
@@ -9,11 +9,11 @@ export function crypto ( str: string, key: string, decode=false ) {
     }
 
     function _x ( str: string ): string {
-        // str = str.replace( /z/g, '=' ).replace( /Z/g, 'z' ).replace( /\=/g, 'Z' );
-        // str = str.replace( /6/g, '=' ).replace( /9/g, '6' ).replace( /\=/g, '9' );
-        // str = str.replace( /n/g, '=' ).replace( /u/g, 'n' ).replace( /\=/g, 'n' );
-        // str = str.replace( /b/g, '=' ).replace( /d/g, 'b' ).replace( /\=/g, 'd' );
-        // str = str.replace( /p/g, '=' ).replace( /q/g, 'p' ).replace( /\=/g, 'q' );
+        str = str.replace( /z/g, '=' ).replace( /Z/g, 'z' ).replace( /=/g, 'Z' );
+        str = str.replace( /6/g, '=' ).replace( /9/g, '6' ).replace( /=/g, '9' );
+        str = str.replace( /n/g, '=' ).replace( /u/g, 'n' ).replace( /=/g, 'n' );
+        str = str.replace( /b/g, '=' ).replace( /d/g, 'b' ).replace( /=/g, 'd' );
+        str = str.replace( /p/g, '=' ).replace( /q/g, 'p' ).replace( /=/g, 'q' );
         return str;
     }
 
