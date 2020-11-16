d160e3124ef95244f7bbb22994c06550a270f9df
Hatef <sz.hatef@gmail.com>
2020-11-16 22:22:22 +0300

    my server

--- src/index.ts
+++ src/index.ts
@@ -148,7 +148,7 @@ app.post( '/crypto_cell', ( req: express.Request, res: express.Response ) => {
 
     // try { queries = JSON.parse( req.body.content ) } catch {}
 
-    return res.json( { answer: JSON.parse( req.body ) } );
+    return res.json( { answer: typeof req.body } );
     // .. validating User
     user._validator( req.body.e as string, req.body.k as string ).then( u => { 
 
