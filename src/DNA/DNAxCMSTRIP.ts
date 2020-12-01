a0a7e4d4fc4b516c5d8f2f9d839878f25a6890f2
Hatef <sz.hatef@gmail.com>
2020-12-01 17:00:21 +0300

    my server

--- src/DNA/DNAxCMSTRIP.ts
+++ src/DNA/DNAxCMSTRIP.ts
@@ -4,7 +4,7 @@ export const DNA: g.gene[] = [
 	{
 		"title"		: "Weird bots",
 		"text"		: "\nI installed a thing so we can see what our visitors are doing in real-time on our site.\n\nAnd there are some real weird sessions...\n\nit seems to be like some kind of bot that clicks on everything without any logic. As they're scanning the site.\n\nOh yeah, it looks like a bot trying to find vulnerabilities in the CMS...\n\nOr maybe they're trying to scrape the site to steal our data?\n\nMaybe.. I'I mention it to CTO!\n\nOK not there..\nNot there either...\nGrrr... it's impossible to understand ANYTHING on this damned website!!\n",
-		"avatarURL"	: "",
+		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/HeadlineImageTemplate-1-2.jpg",
 		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-Drole-de-bot-650-finalenglish.jpg"
 	},
 ]
