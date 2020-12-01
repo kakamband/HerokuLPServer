2c972308a2259eba013fec8599e15a15b4a22f1e
Hatef <sz.hatef@gmail.com>
2020-12-02 00:00:26 +0300

    my server

--- src/DNA/DNAxCMSTRIP.ts
+++ src/DNA/DNAxCMSTRIP.ts
@@ -3,8 +3,21 @@ import * as g                           from '../types/genetics'
 export const DNA: g.gene[] = [
 	{
 		"title"		: "Weird bots",
-		"text"		: "\nI installed a thing so we can see what our visitors are doing in real-time on our site.\n\nAnd there are some real weird sessions...\n\nit seems to be like some kind of bot that clicks on everything without any logic. As they're scanning the site.\n\nOh yeah, it looks like a bot trying to find vulnerabilities in the CMS...\n\nOr maybe they're trying to scrape the site to steal our data?\n\nMaybe.. I'I mention it to CTO!\n\nOK not there..\nNot there either...\nGrrr... it's impossible to understand ANYTHING on this damned website!!\n",
+		"text"		: "I installed a thing so we can see what our visitors are doing in real-time on our site.\n\nAnd there are some real weird sessions...\n\nit seems to be like some kind of bot that clicks on everything without any logic. As they're scanning the site.\n\nOh yeah, it looks like a bot trying to find vulnerabilities in the CMS...\n\nOr maybe they're trying to scrape the site to steal our data?\n\nMaybe.. I'I mention it to CTO!\n\nOK not there..\nNot there either...\nGrrr... it's impossible to understand ANYTHING on this damned website!!\n",
 		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/HeadlineImageTemplate-1-2.jpg",
 		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-Drole-de-bot-650-finalenglish.jpg"
 	},
+	{
+		"title"		: "The best bet for 2030",
+		"text"		: "Have you ever wondered what new web technologies will come out between now md 2030? What we should be betting on right now?\n\nI think in 2030 Node or Python will be leading the pack, or maybe some language that doesn't even exist yet!\n\nThe web moves so fast, we're gonna need to adapt!\n\nDid you know that in 2020, 80% websites are still using PH?, 77% use jQuery and Wordpress has 63% CNS market share?\n\nNo way!\n\nWay. And worse: the numbers are increasing\n\nMaybe it would be best to go all in on PHP/iQuery/Wordpress.,.?",
+		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/12/HeadlineImageTemplate-.jpg",
+		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/12/Strip-Top-langage-web-650-finalenglish.jpg"
+	},
 ]
+
+let t ={
+	"title"		: "",
+	"text"		: "",
+	"avatarURL"	: "",
+	"mediaURL"	: ""
+}
