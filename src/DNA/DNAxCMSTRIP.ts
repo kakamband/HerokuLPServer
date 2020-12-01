46895d3f70125d03895db7c6eceaaf81f328cee1
Hatef <sz.hatef@gmail.com>
2020-12-02 00:33:50 +0300

    my server

--- src/DNA/DNAxCMSTRIP.ts
+++ src/DNA/DNAxCMSTRIP.ts
@@ -2,10 +2,10 @@ import * as g                           from '../types/genetics'
 
 export const DNA: g.gene[] = [
 	{
-		"title"		: "The ‘no-code’ dream…",
-		"text"		: "Hey guys, come look...\n\nI've been waiting so long for this to happen...\n\nWhat's going on?\n\nThe project manager build his entire project on a 'no-coding' platform...\n\nThat's good, he'll learn a thing or two...\n\nYeah, but now he's noticed there's a core feature that can't be done with his tool...\n\nOh...\n\nHe won't admit he's wasted all that time on it, just to have us rebuild the whole thing...\n\nOh no...\n\nLet's go cheer him up...\n\nNo, wait, let him think about his mistake, just for another 2 minutes...",
-		"avatarURL"	: "https://www.commitstrip.com/2020/10/07/the-no-code-dream/",
-		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-PM-et-le-Nocode650-finalenglish.jpg"
+		"title"		: "The best bet for 2030",
+		"text"		: "Have you ever wondered what new web technologies will come out between now and 2030? What we should be betting on right now?\n\nI think in 2030 Node or Python will be leading the pack, or maybe some language that doesn't even exist yet!\n\nThe web moves so fast, we're gonna need to adapt!\n\nDid you know that in 2020, 80% websites are still using PHP, 77% use jQuery and Wordpress has 63% CNS market share?\n\nNo way!\n\nWay. And worse: the numbers are increasing\n\nMaybe it would be best to go all in on PHP/iQuery/Wordpress.,.?",
+		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/12/HeadlineImageTemplate-.jpg",
+		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/12/Strip-Top-langage-web-650-finalenglish.jpg"
 	},
 	{
 		"title"		: "Weird bots",
@@ -14,14 +14,21 @@ export const DNA: g.gene[] = [
 		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-Drole-de-bot-650-finalenglish.jpg"
 	},
 	{
-		"title"		: "The best bet for 2030",
-		"text"		: "Have you ever wondered what new web technologies will come out between now and 2030? What we should be betting on right now?\n\nI think in 2030 Node or Python will be leading the pack, or maybe some language that doesn't even exist yet!\n\nThe web moves so fast, we're gonna need to adapt!\n\nDid you know that in 2020, 80% websites are still using PHP, 77% use jQuery and Wordpress has 63% CNS market share?\n\nNo way!\n\nWay. And worse: the numbers are increasing\n\nMaybe it would be best to go all in on PHP/iQuery/Wordpress.,.?",
-		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/12/HeadlineImageTemplate-.jpg",
-		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/12/Strip-Top-langage-web-650-finalenglish.jpg"
+		"title"		: "The ‘no-code’ dream…",
+		"text"		: "Hey guys, come look...\n\nI've been waiting so long for this to happen...\n\nWhat's going on?\n\nThe project manager build his entire project on a 'no-coding' platform...\n\nThat's good, he'll learn a thing or two...\n\nYeah, but now he's noticed there's a core feature that can't be done with his tool...\n\nOh...\n\nHe won't admit he's wasted all that time on it, just to have us rebuild the whole thing...\n\nOh no...\n\nLet's go cheer him up...\n\nNo, wait, let him think about his mistake, just for another 2 minutes...",
+		"avatarURL"	: "https://www.commitstrip.com/2020/10/07/the-no-code-dream/",
+		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-PM-et-le-Nocode650-finalenglish.jpg"
 	},
+	{
+		"title"		: "Another day, another daily",
+		"text"		: "DAILY MEETING\n\nYou're up!\n\nWhat did you do yesterday, what are you doing today, and what are your problems?\n\nUh... yeah...\n\nYesterday I coded some stuff...\n\nI had some problems, but I solved them pretty quickly...\n\nSo that's it...\n\nGreat\n\nAnd today?\n\nAh... today...\n\nI'm planning to code some other stuff\n\nAnd if I have other problems, I'll solve them too...\n\nAmazing\n\nNEXT!",
+		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/HeadlineImageTemplate-.jpg",
+		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-Daily-meeting-650-finalenglish.jpg"
+	},
+
 ]
 
-let t ={
+let t = {
 	"title"		: "",
 	"text"		: "",
 	"avatarURL"	: "",
