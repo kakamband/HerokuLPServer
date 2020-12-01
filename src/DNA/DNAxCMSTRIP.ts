1b09ede801d973db7e767bc95f4d2e3169b6a08f
Hatef <sz.hatef@gmail.com>
2020-12-02 00:13:25 +0300

    my server

--- src/DNA/DNAxCMSTRIP.ts
+++ src/DNA/DNAxCMSTRIP.ts
@@ -2,6 +2,12 @@ import * as g                           from '../types/genetics'
 
 export const DNA: g.gene[] = [
 	{
+		"title"		: "The ‘no-code’ dream…",
+		"text"		: "Hey guys, come 00k...\n\nI've been waiting so long for this to happen...\n\nWhat's going on?\n\nThe project manager build his entire project on a •no-coding' platform...\n\nYeah, but now he's noticed there's a core feature that can't be done with his tool...\n\nThat's good, he'll beam a thing or two...\n\nHe wont admit he's wasted ail that time on it, just to have us rebuild the whole thing...\n\nOh no...\n\nLet's go cheer him up...\n No, wait, let him think about his mistake, just for another Z minutes...",
+		"avatarURL"	: "https://www.commitstrip.com/2020/10/07/the-no-code-dream/",
+		"mediaURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/Strip-PM-et-le-Nocode650-finalenglish.jpg"
+	},
+	{
 		"title"		: "Weird bots",
 		"text"		: "I installed a thing so we can see what our visitors are doing in real-time on our site.\n\nAnd there are some real weird sessions...\n\nit seems to be like some kind of bot that clicks on everything without any logic. As they're scanning the site.\n\nOh yeah, it looks like a bot trying to find vulnerabilities in the CMS...\n\nOr maybe they're trying to scrape the site to steal our data?\n\nMaybe.. I'I mention it to CTO!\n\nOK not there..\nNot there either...\nGrrr... it's impossible to understand ANYTHING on this damned website!!\n",
 		"avatarURL"	: "https://www.commitstrip.com/wp-content/uploads/2020/10/HeadlineImageTemplate-1-2.jpg",
