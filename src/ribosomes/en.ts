b0210f539464b03983ecd8137371ae6914f73919
szhate <sz.hatef@gmail.com>
2020-12-02 10:21:15 +0300

    my server

--- src/ribosomes/en.ts
+++ src/ribosomes/en.ts
@@ -1,61 +1,61 @@
 import * as g                           from '../types/genetics'
 
 export const en_r: g.Ribosome[] = [
-    {
-        institute   : "en",
-        code        : "EASYRED",
-        type        : "audio",
-        level       : "A2",
-        title       : "Easy\nReading",
-        avatar      : "https://www.gse.harvard.edu/sites/default/files//styles/landscape__1000x500/public/banner/Comics_1500x750.jpg",
-        source      : "LingQ",
-        contains    : null,
-        readMode    : "start"
-    },
-    {
-        institute   : "en",
-        code        : "IIOKGNP",
-        type        : "audio",
-		level       : "A2",
-		title       : "Eating\nOut",
-        avatar      : "https://image.freepik.com/free-vector/people-eating-out-public-place-sitting-outdoors_82574-11144.jpg",
-        source      : "LingQ",
-        contains    : null,
-        readMode    : "start"
-    },
-    {
-        institute   : "en",
-        code        : "ENGSTRY",
-        type        : "audio",
-		level       : "A2",
-		title       : "English\nStories",
-        avatar      : "https://image.shutterstock.com/image-vector/cartoon-sunrise-nature-landscape-mountains-260nw-1015778155.jpg",
-        source      : "LingQ",
-        contains    : null,
-        readMode    : "start"
-    },
-    {
-        institute   : "en",
-        code        : "LTLPRNC",
-        type        : "audio",
-		level       : "B1",
-		title       : "The\nLittle\nPrince",
-        avatar      : "https://static.lingq.com/media/resources/collections/images/2016/08/05/2af63de478_OXQdW5t.jpg",
-        source      : "LingQ",
-        contains    : null,
-        readMode    : "start"
-    },
-    {
-        institute   : "en",
-        code        : "INTSDAY",
-        type        : "audio",
-		level       : "C1",
-		title       : "Interesting\nThing of\nthe Day",
-        avatar      : "https://www.kickassfacts.com/wp-content/uploads/2017/01/fact-of-the-day.jpg",
-        source      : "LingQ",
-        contains    : null,
-        readMode    : "start"
-    },
+    // {
+    //     institute   : "en",
+    //     code        : "EASYRED",
+    //     type        : "audio",
+    //     level       : "A2",
+    //     title       : "Easy\nReading",
+    //     avatar      : "https://www.gse.harvard.edu/sites/default/files//styles/landscape__1000x500/public/banner/Comics_1500x750.jpg",
+    //     source      : "LingQ",
+    //     contains    : null,
+    //     readMode    : "start"
+    // },
+    // {
+    //     institute   : "en",
+    //     code        : "IIOKGNP",
+    //     type        : "audio",
+	// 	level       : "A2",
+	// 	title       : "Eating\nOut",
+    //     avatar      : "https://image.freepik.com/free-vector/people-eating-out-public-place-sitting-outdoors_82574-11144.jpg",
+    //     source      : "LingQ",
+    //     contains    : null,
+    //     readMode    : "start"
+    // },
+    // {
+    //     institute   : "en",
+    //     code        : "ENGSTRY",
+    //     type        : "audio",
+	// 	level       : "A2",
+	// 	title       : "English\nStories",
+    //     avatar      : "https://image.shutterstock.com/image-vector/cartoon-sunrise-nature-landscape-mountains-260nw-1015778155.jpg",
+    //     source      : "LingQ",
+    //     contains    : null,
+    //     readMode    : "start"
+    // },
+    // {
+    //     institute   : "en",
+    //     code        : "LTLPRNC",
+    //     type        : "audio",
+	// 	level       : "B1",
+	// 	title       : "The\nLittle\nPrince",
+    //     avatar      : "https://static.lingq.com/media/resources/collections/images/2016/08/05/2af63de478_OXQdW5t.jpg",
+    //     source      : "LingQ",
+    //     contains    : null,
+    //     readMode    : "start"
+    // },
+    // {
+    //     institute   : "en",
+    //     code        : "INTSDAY",
+    //     type        : "audio",
+	// 	level       : "C1",
+	// 	title       : "Interesting\nThing of\nthe Day",
+    //     avatar      : "https://www.kickassfacts.com/wp-content/uploads/2017/01/fact-of-the-day.jpg",
+    //     source      : "LingQ",
+    //     contains    : null,
+    //     readMode    : "start"
+    // },
     {
         institute   : "en",
         code        : "CMSTRIP",
