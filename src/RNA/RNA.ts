393ed5559d316263e85d43b5d6722a7ebe9ae9fb
szhate <sz.hatef@gmail.com>
2020-12-02 10:03:53 +0300

    my server

--- src/RNA/RNA.ts
+++ src/RNA/RNA.ts
@@ -1,5 +1,3 @@
-import * as x1382                       from "./RNAx1382";
-import * as x1309                       from "./RNAx1309";
 import * as x1127                       from "./RNAx1127";
 import * as x834                        from "./RNAx834";
 import * as x555                        from "./RNAx555";
@@ -14,8 +12,6 @@ interface aRNA {
 } 
 
 export const RNA: { [key: string]: aRNA } = { 
-   x1382,
-   x1309,
    x1127,
    x834,
    x555
