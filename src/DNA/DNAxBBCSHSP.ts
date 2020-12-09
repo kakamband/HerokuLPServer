3f46209b62c5a2ed23274a0eea44cb84308e9da3
szhate <sz.hatef@gmail.com>
2020-12-09 22:43:51 +0300

    my server

--- src/DNA/DNAxBBCSHSP.ts
+++ src/DNA/DNAxBBCSHSP.ts
@@ -11,14 +11,144 @@ export const DNA: g.gene[] = [
 		"title"		: "As dead as a doornail",
 		"text"		: "Narrator: It was a cold morning. William Shakespeare is in the theatre, working on Henry VI part II, whilst his actor friend Thomas Swann is mending a broken door.\nWill: Thomas! Can you stop that banging? I'm trying to write here!\nThomas: Will, this door needs fixing properly. I have to hit this doornail hard… there. That will never come out.\nWill: What's that about a doornail? Hmmm… you've just given me an idea… dead as a doornail… dead as a doornail!\nThomas: Who's dead?! Will?\nWill: Thomas, I have the perfect line for your character… Jack Cade, the rebel leader, strong and proud. But in this scene, he's on the run, hiding from his enemies. He hasn't eaten anything for five days, except herbs… even grass… he's weak… alone… and suddenly, six of his enemies find him.\nThomas: So he's in danger…\nWill: Yes, but he doesn't show any fear: in fact, he threatens to kill all of them dead as a doornail. Come Thomas, let's read your lines. Look on me well: I have eat no meat…\nThomas as Jack Cade: Look on me well: I have eat no meat these five days; yet come thou and thy five men, and if I do not leave you all as dead as a doornail, I pray God I may never eat grass more.\nWill: You have it! Well done Thomas: there's another classic Shakespeare line.\nThomas: I don't know how you do it, Will.\nNarrator: We'll leave them there for now. The phrase as dead as a doornail was in common use in Shakespeare's day. It means the same today as it did back then: Dead. Very dead. Totally dead. It's a very well-known English expression - for example, novelist Charles Dickens used it to describe the character Marley in A Christmas Carol:\nClip 1: Old Marley was as dead as a doornail.\nNarrator: These days we can use it for electrical gadgets that aren't working.\nClip 2: Oh no - I forgot to charge my phone. It's as dead as a doornail.\nWill: Right! All that writing has made me thirsty. Let us go to the Duck and Whistle. Come on Thomas, close the door behind you… Hmmm… perhaps you should put another couple of nails in that door before we go, Thomas?\nThomas: Ahh… To nail, or not to nail: that is the question.",
 		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/1248xn/p03g77bn.jpg",
-		"mediaURL"	: ""
+		"mediaURL"	: "https://downloads.bbc.co.uk/learningenglish/shakespeare/bbc_shakespeare_as_dead_as_a_doornail_download.mp3"
 	},
+	{
+		"title"		: "I'll send him packing",
+		"text"		: "Narrator: It's early morning at the Globe Theatre. William Shakespeare and his actors are rehearsing Henry IV Part 1. In this scene, it's late at night, and young Prince Hal, played by Robert Harley, and his friend Falstaff – that's Thomas Swann – are in the pub. But… the King's messenger has just arrived.\nThomas Swann as Falstaff: … Shall I give him his answer?\nRobert Harley as Prince Hal: Prithee do, Jack.\nThomas Swann as Falstaff: Faith and I'll send him packaged…\nWill: Stoooppppp!!! Thomas, Thomas, it's packing… I'll send him packing… Not packaged. You're not putting him in a box or a parcel. Remember: you want the King's messenger to go away!\nThomas Swann: Sorry, Will… I'm not my best today…\nActor 1: One too many beers last night, eh Thomas?!\nActor 2: He thinks he's Falstaff for real!\nWill: Thomas. Listen to me. Falstaff and Prince Hal are having a great time. That's why Hal doesn't want to see the messenger.\nRobert Harley: Will…\nWill: What is it, Robert?!\nRobert Harley: I do like the messenger's name: Gravity. Gravity! A serious man who doesn't like to have fun. Not like Falstaff. He's…\nActor 1: Old!\nActor 2: …and fat!\nActor 1: He's always telling jokes…\nActor 2: He's never got any money!\nThomas Swann: That last bit's true enough, Will.\nWill: Now, now Thomas – you're perfect for this part. The audience are going to love you! Let's go again, from: What doth Gravity do out of his bed at midnight?\nThomas Swann as Falstaff: What doth Gravity do out of his bed at midnight? Shall I give him his answer?\nRobert Harley as Prince Hal: Prithee do, Jack.\nThomas Swann as Falstaff: Faith, and I'll send him packing!\nWill: Bravo! Let's take a break.\nNarrator: We'll leave them there for now. Falstaff is one of Shakespeare's most popular characters. He drinks, steals and runs away from danger, but Shakespeare’s audiences loved his comic genius and sense of fun. In Henry IV, Prince Hal prefers the company of Falstaff – and his criminal friends – to noblemen. Shakespeare’s phrase I'll send him packing has the same meaning today: I really don't want this person around me, so I'll send them away. It's also used in sport, to talk about beating an opponent. Take Gwyn Jones, former captain of the Welsh rugby team, who said before a match:\nClip 1: This is our chance to do it and we should send them packing with their tails between their legs.\nClip 2: I've no patience when people try to sell me things at the door. I usually send them packing.\nWill: Let's try it once more.\nActor 1: Come on Thomas!\nActor 2: You can do it!\nThomas Swann: Will you all shush?!\nActors 1 & 2: To shush or not to shush, that is the question.",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/1248xn/p03gv0s2.jpg",
+		"mediaURL"	: "https://downloads.bbc.co.uk/learningenglish/shakespeare/bbc_shakespeare_send_him_packing_download.mp3"
+	},
+	{
+		"title"		: "All that glisters is not gold",
+		"text"		: "Narrator: It was a sunny afternoon. William Shakespeare is working on his play The Merchant of Venice. His daughter comes to see him.\nDaughter: Father, look at my new ring! Isn't it lovely, gold and shiny…\nWill: Dear daughter, it is very beautiful. Where did you get such a pretty thing?\nDaughter: From the market. It was much cheaper than the gold merchant!\nWill: Is it real gold?\nDaughter: Yes, of course!\nWill: So, my dear daughter, why is your finger green?\nDaughter: Oohhhh!\nWill: My dear daughter, you have a lot to learn… sit with me while I work. The Merchant of Venice. The Prince of Morocco wants to marry the beautiful Portia. But first, he must choose between three boxes: one made of gold, the second of silver, and the third, of cheap lead. Only one of the boxes contains a picture of Portia, and if the Prince chooses the wrong one, he cannot marry her. So, dear daughter, which box does he choose?\nDaughter: The gold box! Is it real gold?\nWill: He chooses the gold box indeed, and indeed it is real gold. The Prince believes that only the most beautiful box can hold the painting of the beautiful Portia. But in fact, it holds not a picture, but a scroll with these words: All that glisters is not gold…\nRobert Harley as The Prince of Morocco: All that glisters is not gold;: Often have you heard that told:: Many a man his life hath sold: But my outside to behold:\nDaughter: He thought that because the box is beautiful on the outside, something beautiful must be inside… he was wrong.\nNarrator: We'll leave them there for now. The Merchant of Venice is a play about money, money, money, and the phrase all that glisters is not gold warns us not to be fooled by people or things that look good - because they might not be as good as they look on the surface! In modern English, the word glisters is often changed to glistens or glitters. \nClip 1: Well that car looks fantastic, but all that glitters is not gold. Check the engine before you buy it.\nDaughter: Oh father, will you buy me a real gold ring? Pleeeeeaase?\nWill: Hmmm… to buy, or not to buy: that is the question.",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/1248xn/p03f264d.jpg",
+		"mediaURL"	: "http://downloads.bbc.co.uk/learningenglish/shakespeare/bbc_shakespeare_all_that_glisters_download.mp3"
+	},
+	{
+		"title"		: "Strange bedfellows",
+		"text"		: "Narrator: It's late in the evening. William Shakespeare is visiting his actor friend Robert Harley.\nRobert: Will! Just one moment… I have to rescue the cat. It likes to sleep with the chickens and it gets locked in the henhouse… come on kitty…\nWill: The cat sleeps with the chickens? That's unusual.\nRobert Harley: It's strange, but they seem quite happy together. So, Will, I've read your new play The Tempest and I'm very excited about all the magic, the music and the monsters, and my character, Trinculo, the lost traveller: it's a wonderful part. His meeting with Caliban: it's very interesting.\nWill: Ah, yes, Caliban. Neither man nor fish – a weird sort of creature.\nRobert Harley: You put them both in a storm…\nWill: Yes: it's an old trick but it's a good way to bring them together. Trinculo needs shelter and the strange, well, almost monstrous Caliban is wearing a gaberdine: a large coat, big enough for both of them. Trinculo gets under it, safe and warm from the storm.\nRobert Harley: But Trinculo isn't happy…\nWill: He's feeling very miserable. He actually says: Alas, the storm is come again! My best way…\nRobert Harley as Trinculo: Alas, the storm is come again! My best way is to creep under his gaberdine; there is no other shelter hereabouts: misery acquaints a man with strange bedfellows.\nWill: Ohh, I do like that line: Misery acquaints a man with strange bed-fellows. When times are hard, people do things they don't normally do…\nNarrator: We'll leave them there for now. Shakespeare lived in a time of discovery – strange new lands and creatures, so the mysterious island of The Tempest appealed to Shakespeare's audience as both exciting and scary. These days, the phrase strange bedfellows describes two people or groups that are connected in a particular activity, even though they are very different and are not usually seen together. It's often used for political alliances. For example, a July 2015 report from US broadcaster Fox News described Israel and Hamas as strange bedfellows when they both wanted to stop the growth of so-called Islamic State in Gaza. The headline was:\nClip 1: Israel, Hamas strange bedfellows when it comes to reining in ISIS in Gaza.\nNarrator: Strange bedfellows is useful for all sorts of unlikely partnerships.\nClip 2: You think Miley Cyrus and Michael Bublé should write a song together? Well, they'd be strange bedfellows… but it might just work.\nRobert Harley: Now Will, it's getting late and there's a storm coming. You must stay with us tonight. You can sleep in the henhouse – or you can share a bed with the Harley family.\nWill: Hmmm… To bed or not to bed: that is the question.",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/1248xn/p03hxy1c.jpg",
+		"mediaURL"	: "https://downloads.bbc.co.uk/learningenglish/shakespeare/bbc_shakespeare_strange_bedfellows_download.mp3"
+	},
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
+	// {
+	// 	"title"		: "",
+	// 	"text"		: "",
+	// 	"avatarURL"	: "",
+	// 	"mediaURL"	: ""
+	// },
 ]
-
-
-// {
-// 	"title"		: "",
-// 	"text"		: "",
-// 	"avatarURL"	: "",
-// 	"mediaURL"	: ""
-// },
