0ec08ecd99647f757e9cc79692c7f02dd6294193
szhate <sz.hatef@gmail.com>
2020-12-10 08:58:19 +0300

    my server

---
+++ src/DNA/DNAxBBCCHCO.ts
@@ -0,0 +1,59 @@
+import * as g                           from '../types/genetics'
+
+export const DNA: g.gene[] = [
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "",
+		"text"		: "",
+		"avatarURL"	: "",
+		"mediaURL"	: ""
+	},
+	
+]
