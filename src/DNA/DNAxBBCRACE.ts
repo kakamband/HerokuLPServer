87e7630b4648dac2f831323f1b44df06d7d0fdb8
szhate <sz.hatef@gmail.com>
2020-12-10 11:43:20 +0300

    my server

---
+++ src/DNA/DNAxBBCRACE.ts
@@ -0,0 +1,64 @@
+import * as g                           from '../types/genetics'
+
+export const DNA: g.gene[] = [
+	{
+		"title"		: "Episode 1",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p029ykrb.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 2",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p029ylrs.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 3",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p029yn74.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 4",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p02745s2.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 5",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p028kxkr.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 6",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p024crgd.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 7",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p029z2pd.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 8",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p024844w.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 9",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p02b0sm8.jpg",
+		"mediaURL"	: ""
+	},
+	{
+		"title"		: "Episode 10",
+		"text"		: "",
+		"avatarURL"	: "https://ichef.bbci.co.uk/images/ic/320x180/p02cths4.jpg",
+		"mediaURL"	: ""
+	},
+]
